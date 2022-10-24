47b30e001f8b262a17fc54a4e7dca226cf058b27
Kyle Roberts <kyle@philliptown.dev>
2022-10-24 08:06:40 -0400

    Refactor: add api_key, projectData & setup Get & Post data routes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-// Get Environment Variables
+// Get Environment Variables, Setup Weather API Key & Project Data
 require('dotenv').config();
+const api_key = process.env.WEATHER_API_KEY;
+const projectData = {};
 
 // Get Dependencies
 const express = require('express');
@@ -8,17 +10,32 @@ const cors = require('cors');
 // Initialize Express
 const app = express();
 
-// Set up CORS
+// Express use CORS
 app.use(cors());
 
-// Set up Express to handle data parsing
+// Express parse URL encoded data & JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set up Express to serve static files
+// Express serve static files
 app.use(express.static('public'));
 
-// Set up Express to listen on port 3000
+// Routes
+
+// Get Data Route
+app.get('/data', (req, res) => {
+    res.send(projectData);
+});
+
+// Post Data Route
+app.post('/data', (req, res) => {
+    let data = req.body;
+    projectData.temp = data.temp;
+    projectData.date = data.date;
+    projectData.userResponse = data.userResponse;
+});
+
+// Express listen on port 3000
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
